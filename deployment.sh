0d34a308d75987563c6b04d6bd3924e51e9b3e9b
smetal1 <saurav7055@gmail.com>
2018-09-02 14:02:17 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -2,4 +2,4 @@ docker login -u saurav705555 -p sauravkumar21
 docker build . -t saurav705555/sampleapp:latest
 docker push saurav705555/sampleapp:latest
 
-oc tag --source=docker saurav705555/sampleapp:latest sampleapp:latest
+sudo oc tag --source=docker saurav705555/sampleapp:latest sampleapp:latest

fdadd262656fe14bef664162bc0fc6718e22901a
smetal1 <saurav7055@gmail.com>
2018-09-02 10:54:32 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,8 @@
 oc patch route/bluegreen-test-app -p '{"spec":{"to":{"name":"test-app-blue"}}}'
 oc delete all --selector app=test-app-green
 oc new-app . --strategy=docker --name=test-app-green
+sleep 30s
 oc expose svc/test-app-green --name=bluegreen-test-app
+sleep  30s
 oc delete all --selector app=test-app-blue
 oc new-app . --strategy=docker --name=test-app-blue
